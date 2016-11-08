8cc75eb6043895a21668800dcb1ec3d48afe287a
Valery Tschopp <valery.tschopp@switch.ch>
2016-11-08 17:09:57 +0100

    new format for the images information

--- files/distrosInfo.py
+++ files/distrosInfo.py
@@ -1,113 +1,134 @@
-global distrosMap, descriptionsMap, description_old
+#
+# images information config
+#
+image_description_deprecated = "DEPRECATED, please use the most recent public image"
 
-descriptionText = "Please refer to http://help.switch.ch/engines/documentation/switch-official-images for further information"
-description_old = "Deprecated, please use the most recent version, with property Public"
+images_info = {}
 
-distrosMap = {}
-
-# template: distrosMap["distrosKey"]=[diskimage-builder target e.g. debian, image name, DIB_DISTRIBUTION_MIRROR,DIB_RELEASE,PATH,DESCRIPTION]
-
-distrosMap["centos_7"] = {
-	"target": "centos7",
+images_info["centos_7"] = {
+    "image_raw_source": "centos7.raw",
 	"image_name": "CentOS 7.2 (SWITCHengines)",
-	"dib_distribution_mirror": "http://mirror.switch.ch/ftp/mirror/centos/",
-	"dib_release": "",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "centos",
-	"default_user": "centos",
-	"os_version": "7.2",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "512",
+    "image_min_disk": "auto",
+	"image_properties": {
+    	"description": "Username: 'centos'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "centos",
+	    "default_user": "centos",
+	    "os_version": "7.2",
+	    "requires_rdp": "false",
+	    "requires_ssh": "true"
+    }
 }
 
-distrosMap["debian_jessie"] = {
-	"target": "debian",
+images_info["debian_jessie"] = {
+    "image_raw_source": "debianJessie.raw",
 	"image_name": "Debian Jessie 8.2 (SWITCHengines)",
-	"dib_distribution_mirror": "http://ftp.ch.debian.org/debian/",
-	"dib_release": "Jessie",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "debian",
-	"default_user": "debian",
-	"os_version": "Jessie 8.6",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "512",
+    "image_min_disk": "auto",
+	"image_properties": {
+    	"description": "Username: 'debian'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "debian",
+        "default_user": "debian",
+        "os_version": "Jessie 8.6",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
 
-distrosMap["debian_wheezy"] = {
-	"target": "debian",
+images_info["debian_wheezy"] = {
+    "image_raw_source": "debianWheezy.raw",
 	"image_name": "Debian Wheezy 7.11 (SWITCHengines)",
-	"dib_distribution_mirror": "http://ftp.ch.debian.org/debian/",
-	"dib_release": "Wheezy",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "debian",
-	"default_user": "debian",
-	"os_version": "Wheezy 7.11",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "512",
+    "image_min_disk": "auto",
+	"image_properties": {
+        "description": "Username: 'debian'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "debian",
+        "default_user": "debian",
+        "os_version": "Wheezy 7.11",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
 
-distrosMap["fedora_22"] = {
-	"target": "fedora",
+images_info["fedora"] = {
+    "image_raw_source": "fedora.raw",
 	"image_name": "Fedora release 22 (SWITCHengines)",
-	"dib_distribution_mirror": "http://mirror.switch.ch/ftp/mirror/fedora/linux",
-	"dib_release": "",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "fedora",
-	"default_user": "fedora",
-	"os_version": "22",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "512",
+    "image_min_disk": "auto",
+	"image_properties": {
+        "description": "Username: 'fedora'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "fedora",
+        "default_user": "fedora",
+        "os_version": "22",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
 
-distrosMap["ubuntu_trusty"] = {
-	"target": "ubuntu",
+images_info["ubuntu_trusty"] = {
+    "image_raw_source": "ubuntu.raw",
 	"image_name": "Ubuntu Trusty 14.04 (SWITCHengines)",
-	"dib_distribution_mirror": "http://ch.archive.ubuntu.com/ubuntu",
-	"dib_release": "",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "ubuntu",
-	"default_user": "ubuntu",
-	"os_version": "Trusty 14.04",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "512",
+    "image_min_disk": "auto",
+	"image_properties": {
+        "description": "Username: 'ubuntu'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "ubuntu",
+        "default_user": "ubuntu",
+        "os_version": "Trusty 14.04",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
 
-distrosMap["ubuntu_xenial"] = {
-	"target": "ubuntu",
+images_info["ubuntu_xenial"] = {
+    "image_raw_source": "ubuntuxenial.raw",
 	"image_name": "Ubuntu Xenial 16.04 (SWITCHengines)",
-	"dib_distribution_mirror": "http://ch.archive.ubuntu.com/ubuntu",
-	"dib_release": "",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "ubuntu",
-	"default_user": "ubuntu",
-	"os_version": "Xenial 16.04",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "512",
+    "image_min_disk": "auto",
+    "image_properties": {
+        "description": "Username: 'ubuntu'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "ubuntu",
+        "default_user": "ubuntu",
+        "os_version": "Xenial 16.04",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
 
-distrosMap["rstudio"] = {
-	"target": "ubuntu",
+images_info["rstudio"] = {
+    "image_raw_source": "rstudio.raw",
 	"image_name": "RStudio Appliance (SWITCHengines)",
-	"dib_distribution_mirror": "http://ch.archive.ubuntu.com/ubuntu",
-	"dib_release": "",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "ubuntu",
-	"default_user": "ubuntu",
-	"os_version": "Trusty 14.04",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "1024",
+    "image_min_disk": "auto",
+   	"image_properties": {
+        "description": "Username: 'ubuntu'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "ubuntu",
+        "default_user": "ubuntu",
+        "os_version": "Trusty 14.04",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
 
-
-distrosMap["zeppelin"] = {
-	"target": "ubuntu",
+images_info["zeppelin"] = {
+    "image_raw_source": "",
 	"image_name": "Spark Zeppelin (SWITCHengines)",
-	"dib_distribution_mirror": "http://ch.archive.ubuntu.com/ubuntu",
-	"dib_release": "",
-	"path": "/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/ubuntu/diskimage-builder/bin:/home/ubuntu/dib-utils/bin/",
-	"os_flavor": "ubuntu",
-	"default_user": "ubuntu",
-	"os_version": "Trusty 14.04",
-	"requires_rdp": "false",
-	"requires_ssh": "true"
+    "image_min_ram": "4096",
+    "image_min_disk": "auto",
+    "image_properties": {
+        "description": "Username: 'ubuntu'. See http://help.switch.ch/engines/documentation/switch-official-images for further information",
+        "hw_vif_multiqueue_enabled": "true",
+        "os_flavor": "ubuntu",
+        "default_user": "ubuntu",
+        "os_version": "Trusty 14.04",
+        "requires_rdp": "false",
+        "requires_ssh": "true"
+    }
 }
